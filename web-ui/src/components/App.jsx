8083b8561b64b9e792bb09517aada556474884e5
Apprentice76 <hrk.som.01@gmail.com>
2022-02-16 01:30:53 +0530

    basic-ui

--- web-ui/src/components/App.jsx
+++ web-ui/src/components/App.jsx
@@ -1,11 +1,33 @@
-
+import QRCode from 'react-qr-code'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import '../styles/App.css'
 
 const App = () => {
-  return (
-    <div className="App">
-      <h1>Start!</h1>
-    </div>
-  );
+	const [accessToken, setAccessToken] = useState(null)
+
+	useEffect(() => {
+		const url = `${process.env.REACT_APP_BACKER_URL}/getqr`
+
+		axios
+			.get(url)
+			.then((resp) => setAccessToken(resp.data.access_token))
+			.catch((err) => console.log(err))
+	}, [])
+
+	return (
+		<div className='main'>
+			<h1>Test Page</h1>
+			<div className='code_container'>
+				<div className='qrcode'>
+					<QRCode value={accessToken ? accessToken : 'test-data'} className='qrcode_element'/>
+				</div>
+			</div>
+			<div className='description'>
+				Scan the QR code using client app.
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
